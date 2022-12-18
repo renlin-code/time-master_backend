a1b12c5121c7c30227ad7b6db43b63bd6a09ed54
renlin-code <renelj1997@gmail.com>
2022-12-18 10:18:04 +0500

    All direct routes hidden

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 
-const usersRoutes = require("./users.routes");
-const categoriesRoutes = require("./categories.routes");
-const tasksRoutes = require("./tasks.routes");
+// const usersRoutes = require("./users.routes");
+// const categoriesRoutes = require("./categories.routes");
+// const tasksRoutes = require("./tasks.routes");
 const authRoutes = require("./auth.routes");
 const profileRoutes = require("./profile.routes");
 
@@ -10,9 +10,9 @@ function routerApi(app) {
     const router = express.Router();
     app.use('/api/v1', router);
 
-    router.use("/users", usersRoutes);
-    router.use("/categories", categoriesRoutes);
-    router.use("/tasks", tasksRoutes);
+    // router.use("/users", usersRoutes);
+    // router.use("/categories", categoriesRoutes);
+    // router.use("/tasks", tasksRoutes);
     router.use("/auth", authRoutes);
     router.use("/profile", profileRoutes);
 }
